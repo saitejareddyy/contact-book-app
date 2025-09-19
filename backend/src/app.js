f36f35324238ed53ce857aec5a4c7ff511387b17
saitejareddyy <saitejareddy0502@gmail.com>
2025-09-20 02:50:35 +0530

    Update app.js
    added deployed frontend url in cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true, limit: "20kb"}))
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://contact-book-app-frontend.onrender.com/",
     credentials: true,
   })
 );
